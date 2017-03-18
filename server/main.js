732808969e51e77e332f2986bc968720e533d638
ronnyr <ronnyr@wix.com>
2017-03-18 17:24:24 +0200

    add server

---
+++ server/main.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!1111');
+});
+
+app.post('/alarms/new', jsonParser, (req, res) => {
+	res.send(req.body);
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
